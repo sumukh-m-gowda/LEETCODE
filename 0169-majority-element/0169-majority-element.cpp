428e5e71e6db24fee48a0fa144d5a0e8477fccd2
sumukh m gowda <sumukh.m2006@gmail.com>
2026-08-11 21:35:21 +0530

    Time: 0 ms (100%), Space: 28.4 MB (14%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin() , nums.end());
-        int count = 0; 
-        int n = nums.size();
-        if(n == 1) {
-            return nums[0];
-        }
-        for (int i = 1 ; i < nums.size()  ; i++) {
-            if(nums[i - 1] == nums[i]){
+        // sort(nums.begin() , nums.end());
+        // int count = 0; 
+        // int n = nums.size();
+        // if(n == 1) {
+        //     return nums[0];
+        // }
+        // for (int i = 1 ; i < nums.size()  ; i++) {
+        //     if(nums[i - 1] == nums[i]){
+        //         count++;
+        //     } else {
+        //         count = 0 ;
+        //     }
+        //     if(count >= n/2) {
+        //         return nums[i];
+        //     }
+        // }
+        // return 0 ;
+
+        int can = 0 ;
+        int count =0 ;
+        for (int i = 0 ; i < nums.size() ; i++) {
+            if(count == 0) {
+                can = nums[i];
+            }
+            if(can == nums[i]){
                 count++;
             } else {
-                count = 0 ;
-            }
-            if(count >= n/2) {
-                return nums[i];
+                 count--;
             }
         }
-        return 0 ;
+        return can;
     }
 };
